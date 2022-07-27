f6aebf8a0539d08f9452446a18d23e1af3a403dc
chanhi <61691088+chanhi@users.noreply.github.com>
2022-07-27 17:38:44 +0900

    error: Do not find snapshot doc

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,4 +1,76 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { dbService } from "../firebase";
+import {
+  addDoc,
+  collection,
+  onSnapshot,
+  orderBy,
+  query,
+} from "firebase/firestore";
 
-const Home = () => <span>Home</span>;
+const Home = ({ userObj }) => {
+  const [nweet, setNweet] = useState("");
+  const [nweets, setNweets] = useState([]);
+  useEffect(() => {
+    const q = query(
+      collection(dbService, "nweets"),
+      orderBy("createAt", "desc")
+    );
+    console.log(q);
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      console.log(querySnapshot);
+      const newArray = querySnapshot.docs.map((doc) => {
+        return {
+          id: doc.id,
+          ...doc.data(),
+        };
+      });
+      setNweets(newArray);
+      console.log(newArray);
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      await addDoc(collection(dbService, "nweets"), {
+        text: nweet,
+        createdAt: Date.now(),
+        creatorId: userObj.uid,
+      });
+    } catch (error) {
+      console.error("Error adding document: ", event);
+    }
+    setNweet("");
+  };
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setNweet(value);
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          value={nweet}
+          onChange={onChange}
+          type="text"
+          placeholder="What's on your mind?"
+          maxLength={120}
+        />
+        <input type="submit" value="Nweet" />
+      </form>
+      <div>
+        {nweets.map((nweet) => (
+          <div key={nweet.id}>
+            <h4>{nweet.text}</h4>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 export default Home;
